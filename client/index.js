d43b8ea62cbf38ec3dcee6f253e92dad0758c62a
Tushaar Rohatgi <hitushaar@gmail.com>
2025-04-24 23:39:01 +0530

    second commit

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+import readline from "readline/promises";
+import { GoogleGenAI } from "@google/genai";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
